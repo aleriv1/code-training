11da993e074a25bc10ece57f8e16552f2e6603ca
Aleksandr <aleksandr_ermakov@outlook.com>
2025-12-16 19:47:13 +0300

    loop repeated

--- module-js-base/js-introduction/tasks/JsIntroTasks.js
+++ module-js-base/js-introduction/tasks/JsIntroTasks.js
@@ -27,7 +27,7 @@ function jsIntroTask1() {
   }
 }
 
-jsIntroTask1()
+// jsIntroTask1()
 
 function jsIntroTask1Mentor() {
   const existingUserLogin = "the_best_user";
@@ -48,6 +48,180 @@ function jsIntroTask1Mentor() {
 
 // #region jsintroTask2
 
+function jsIntroTask2() {
+  const correctAnswer1 = 4
+  const correctAnswer2 = 4
+  const correctAnswer3 = 1
+  const correctAnswer4 = 12
+  const correctAnswer5 = 6
 
+  let correctAnswers = 0
+  let incorrectAnswers = 0
+
+  // let userAnswer1 = 4
+  let userAnswer1 = +prompt('What is 2+2')
+  // userAnswer1 === correctAnswer1 ? (console.log(`You're right`), correctAnswers++) : (console.log(`You're not right`), incorrectAnswers++)
+  userAnswer1 === correctAnswer1 ? (alert(`You're right`), correctAnswers++) : (alert(`You're not right`), incorrectAnswers++)
+  // let userAnswer2 = 2
+  let userAnswer2 = +prompt('What is 2*2')
+  // userAnswer2 === correctAnswer2 ? (console.log(`You're right`), correctAnswers++) : (console.log(`You're not right`), incorrectAnswers++)
+  userAnswer2 === correctAnswer2 ? (alert(`You're right`), correctAnswers++) : (alert(`You're not right`), incorrectAnswers++)
+  // let userAnswer3 = 1
+  let userAnswer3 = +prompt('Petya had 5 apples. He ate 3 and gave one to his friend. How many apples does Petya have left?')
+  // userAnswer3 === correctAnswer3 ? (console.log(`You're right`), correctAnswers++) : (console.log(`You're not right`), incorrectAnswers++)
+  userAnswer3 === correctAnswer3 ? (alert(`You're right`), correctAnswers++) : (alert(`You're not right`), incorrectAnswers++)
+  // let userAnswer4 = 12
+  let userAnswer4 = +prompt('Mashs has 10 candies. She ate 2, then gave 1 to her friend. After that her Mom gave her 5 more candies. How many candies did Masha have left in the end')
+  // userAnswer4 === correctAnswer4 ? (console.log(`You're right`), correctAnswers++) : (console.log(`You're not right`), incorrectAnswers++)
+  userAnswer4 === correctAnswer4 ? (alert(`You're right`), correctAnswers++) : (alert(`You're not right`), incorrectAnswers++)
+  // let userAnswer5 = 6
+  let userAnswer5 = +prompt('What is 2+2*2')
+  // userAnswer5 === correctAnswer5 ? (console.log(`You're right`), correctAnswers++) : (console.log(`You're not right`), incorrectAnswers++)
+  userAnswer5 === correctAnswer5 ? (alert(`You're right`), correctAnswers++) : (alert(`You're not right`), incorrectAnswers++)
+
+
+
+  // console.log(`End of test; the correct answers: ${correctAnswers}, the incorrect answers: ${incorrectAnswers}`)
+  alert(`End of test; the correct answers: ${correctAnswers}, the incorrect answers: ${incorrectAnswers}`)
+}
+
+function jsintroTask2Mentor() {
+  const question1 = "Сколько будет 2 + 2?";
+  const question2 = "Сколько будет 2 * 2?";
+  const question3 =
+    "У Пети было 5 яблок. 3 из них он съел, 1 отдал другу. Сколько яблок у Пети осталось?";
+  const question4 =
+    "У Маши было 10 конфет. 2 она съела, 1 отдала другу. После мама дала Маше еще 5 конфет. Сколько в итоге конфет осталось у Маши?";
+  const question5 = "Сколько будет 2 + 2 * 2?";
+
+  const answer1 = 4;
+  const answer2 = 4;
+  const answer3 = 1;
+  const answer4 = 12;
+  const answer5 = 6;
+
+  let correctAnswers = 0;
+  let incorrectAnswers = 0;
+
+  const userAnswer1 = prompt(question1);
+  if (Number(userAnswer1) === answer1) {
+    alert("Ответ Верный");
+    correctAnswers += 1;
+  } else {
+    alert("Ответ Неверный");
+    incorrectAnswers += 1;
+  }
+
+  const userAnswer2 = prompt(question2);
+  if (Number(userAnswer2) === answer2) {
+    alert("Ответ Верный");
+    correctAnswers += 1;
+  } else {
+    alert("Ответ Неверный");
+    incorrectAnswers += 1;
+  }
+
+  const userAnswer3 = prompt(question3);
+  if (Number(userAnswer3) === answer3) {
+    alert("Ответ Верный");
+    correctAnswers += 1;
+  } else {
+    alert("Ответ Неверный");
+    incorrectAnswers += 1;
+  }
+
+  const userAnswer4 = prompt(question4);
+  if (Number(userAnswer4) === answer4) {
+    alert("Ответ Верный");
+    correctAnswers += 1;
+  } else {
+    alert("Ответ Неверный");
+    incorrectAnswers += 1;
+  }
+
+  const userAnswer5 = prompt(question5);
+  if (Number(userAnswer5) === answer5) {
+    alert("Ответ Верный");
+    correctAnswers += 1;
+  } else {
+    alert("Ответ Неверный");
+    incorrectAnswers += 1;
+  }
+
+  alert(
+    `Конец теста! Правильные ответы - ${correctAnswers}; Неправильные ответы - ${incorrectAnswers}.`
+  );
+
+}
+
+
+// jsIntroTask2()
 
 // #endregion jsintroTask2
+
+// #region jsIntroTask 4 loops while and do while
+
+function jsIntroTask4OriLoop() {
+  for (let i = 0; i < 3; i += 1) {
+    let newStudent = prompt('Введите имя нового студента!');
+    if (newStudent) {
+      newStudent = newStudent.trim();
+      alert(`Добро пожаловать, ${newStudent}!`)
+    }
+  }
+}
+
+function jsIntroTask4DoWhile() {
+  let i = 0
+  do {
+    let newStudent = prompt('Введите имя нового студента!');
+    if (newStudent) {
+      newStudent = newStudent.trim();
+      alert(`Добро пожаловать, ${newStudent}!`)
+    }
+    i++
+  } while (i < 3)
+}
+
+function jsIntroTask4While() {
+  let i = 0
+  while (i < 3) {
+    // let newStudent = prompt('enter the new student name')
+    let newStudent = prompt('Введите имя нового студента!')
+    if (newStudent) {
+      newStudent = newStudent.trim();
+      alert(`Добро пожаловать, ${newStudent}!`)
+    }
+    i++
+  }
+}
+
+
+// #endregion jsIntroTask 4 loops while and do while
+
+// #region jsIntroTask5 loops, the all numbers sum
+
+function jsInto5TheAllNumbersSumWhile() {
+  let sum = 0
+  let i = 1
+  while (i <= 100) {
+    sum += i
+    i++
+  }
+  console.log(sum)
+}
+
+jsInto5TheAllNumbersSumWhile()
+
+function jsInto5TheAllNumbersSumFor() {
+  let sum = 0
+  for (let i = 1; i <= 100; i++) {
+    sum += i
+  }
+
+  console.log(sum)
+}
+
+jsInto5TheAllNumbersSumFor()
+
+// #endregion jsIntroTask5 loops, the all numbers sum
---
+++ module-js-base/js-introduction/tasks/confirm/confirm.js
@@ -0,0 +1,38 @@
+// let confirmQTest = confirm()
+// console.log(confirmQTest)
+
+function confirmTask() {
+  confirm('js appeared in 1995?') ? console.log('right') : console.log('wrong')
+  confirm('is the js specification called ECMAscript') ? console.log('right') : console.log('wrong')
+  confirm('Was js created in one month') ? console.log('right') : console.log('wrong')
+}
+
+// confirmTask()
+
+
+function confirmTaskMentor() {
+  /* eslint-disable */
+
+  if (confirm("JavaScript появился в 1995 году?") === true) {
+    alert("Верно!");
+  } else {
+    alert("Неверно! JavaScript появился в 1995 году");
+  }
+
+  if (confirm("Спецификация JavaScript называется ECMAScript?") === true) {
+    alert("Верно!");
+  } else {
+    alert("Неверно! Спецификация JavaScript называется ECMAScript");
+  }
+
+  if (confirm("JavaScript был создан за 1 месяц?") === true) {
+    alert("Неверно! JavaScript был создан за 10 дней");
+  } else {
+    alert("Верно! JavaScript был создан за 10 дней");
+  }
+}
+
+
+// if (confirm()) {
+//   console.log('confirm')
+// }
