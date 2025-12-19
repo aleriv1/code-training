90d5697a7d83284387c7d59b44151cf5c17ed4aa
Aleksandr <aleksandr_ermakov@outlook.com>
2025-12-19 07:14:17 +0300

    name changing

--- module-js-base/hello-world/src/index.js
+++
@@ -1,5 +0,0 @@
-console.log('hello')
-console.log('world')
-alert('hi')
-
-prompt('what', 'when')
--- module-js-base/js-introduction/tasks/index0.js
+++
@@ -1,5 +0,0 @@
-const rate = 51 // the course ofr $
-const amount = 100
-
-const result = Number((amount / rate).toFixed(2))
-console.log(`${amount}r = $${result}`)
