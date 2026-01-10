52f4956a5d15842b96835ccfd0be6be96badba4e
Aleksandr <aleksandr_ermakov@outlook.com>
2026-01-10 14:24:30 +0300

    free playground

--- free-playground/app.js
+++ free-playground/app.js
@@ -1,18 +1,51 @@
-class ArrayForFly {
+class classForArray {
   constructor(name, animal, expirience) {
     this.name = name;
     this.animal = animal;
     this.expirience = expirience;
   }
+
+  getInfo() {
+    const infoStriing = `name is: ${this.name}; animal is: ${this.animal}; expirience is: ${this.expirience}`;
+    return infoStriing;
+  }
 }
 
 // const obj = new ArrayForFly("Aleks", "tiger", 39);
 
-console.log(new ArrayForFly("Aleks", "tiger", 39));
+// console.log(new ArrayForFly("Aleks", "tiger", 39));
 
 const arrayToPlay = [
-  new ArrayForFly("Aleks", "tiger", 39),
-  new ArrayForFly("Alena", "panther", 26),
+  new classForArray("Aleksand", "tiger", 39),
+  new classForArray("Alena", "panther", 26),
 ];
 
+console.log(arrayToPlay[0].getInfo());
+
+arrayToPlay.forEach((person) => {
+  person.connection = true;
+});
+
 console.log(arrayToPlay);
+
+const mapArrayToPlay = arrayToPlay.map((person) => {
+  return { ...person, name: person.name.slice(0, -3) };
+});
+
+console.log(mapArrayToPlay);
+
+// acc.push(...Object.values(el));
+const reduceArrayToPlay = arrayToPlay.reduce((acc, el) => {
+  // acc.push(...Object.values(el));
+  // acc["name"] += ` ${el.name}`;
+  Object.keys(el).forEach((key) => {
+    if (key === "name") {
+      acc[key] += `${el[key]}`;
+      return;
+    }
+    acc[key] += ` ${el[key]}`;
+  });
+  return acc;
+});
+
+console.log(reduceArrayToPlay);
