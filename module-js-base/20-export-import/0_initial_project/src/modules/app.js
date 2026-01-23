9621c8846aac3ad393e3093804554735d8d7eda5
Aleksandr <aleksandr_ermakov@outlook.com>
2026-01-23 22:06:53 +0300

    import and export, lesson 2, begin

--- module-js-base/20-export-import/0_initial_project/src/modules/app.js
+++ module-js-base/20-export-import/0_initial_project/src/modules/app.js
@@ -1,7 +1,7 @@
 import { JSblock } from "./js-block"
 
-export default class App { // default must be the only one in one file
-  #JSblock // private because it doesn't need to have access through a dot
+export default class App {
+  #JSblock
 
   constructor() {
     this.#JSblock = new JSblock()
@@ -10,7 +10,5 @@ export default class App { // default must be the only one in one file
   run() { // this method we should import in index js
     const jsBlockHTML = this.#JSblock.render()
     document.body.append(jsBlockHTML)
-
-    // document.body.textContent = 'Hello World!'
   }
 }
