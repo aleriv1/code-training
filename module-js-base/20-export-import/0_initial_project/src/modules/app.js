759c7c4d29fb1deb33ee8e5da6b778304b390536
Aleksandr <aleksandr_ermakov@outlook.com>
2026-01-23 20:42:51 +0300

    import and export, lesson 2, JSBlock,  begin

--- module-js-base/20-export-import/0_initial_project/src/modules/app.js
+++ module-js-base/20-export-import/0_initial_project/src/modules/app.js
@@ -1,17 +1,5 @@
-// class App {
-// export class App { // The number of regular exports can be as many as needed
 export default class App { // default must be the only one in one file
   run() { // this method we should import in index js
     document.body.textContent = 'Hello World!'
   }
 }
-
-// export function createFunc() {
-//   console.log('Alena')
-// }
-
-// export {
-//   App
-// }
-
-// export default App
